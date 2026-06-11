c4caeb456247764fd6081e1ba0301cff27a3b580
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-11 22:50:28 +0530

    ✅ Leetcode: Plus One [Easy] (Python3)

---
+++ leetcode/easy/0066-plus-one.py
@@ -0,0 +1,24 @@
+# ===================================================
+# Problem  : Plus One
+# Platform : Leetcode
+# Link     : https://leetcode.com/problems/plus-one/
+# Difficulty: Easy
+# Language : Python3
+# Runtime  : 0 ms
+# Memory   : 19.1 MB
+# Date     : June 11, 2026
+# ===================================================class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+         for i in range(len(digits) - 1, -1, -1):
+
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+            if i == 0:
+                return [1] + digits
+
+        
