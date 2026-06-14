82b0353484a3019d715522df48f904605e12031e
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-14 23:23:59 +0530

    ✅ Leetcode: Power of Two [Easy] (Python3)

---
+++ leetcode/easy/0231-power-of-two.py
@@ -0,0 +1,13 @@
+# ===================================================
+# Problem  : Power of Two
+# Platform : Leetcode
+# Link     : https://leetcode.com/problems/power-of-two/
+# Difficulty: Easy
+# Language : Python3
+# Runtime  : 0 ms
+# Memory   : 19.2 MB
+# Date     : June 14, 2026
+# ===================================================class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and not (n & (n - 1))
+        
