84eaa9c56f723fb76c4ab8c523d4e190f07790f9
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-18 22:51:30 +0530

    ✅ Leetcode: Concatenation of Array [Easy] (Python3)

--- leetcode/easy/2058-concatenation-of-array.py
+++ leetcode/easy/2058-concatenation-of-array.py
@@ -6,10 +6,11 @@
 # Language : Python3
 # Runtime  : 0 ms
 # Memory   : 19.3 MB
-# Date     : June 4, 2026
+# Date     : June 18, 2026
 # ===================================================class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         result=[]
         
         result = nums*2
         return result
+        
