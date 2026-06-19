d4eaa8f1ad1c6e25025c19eda5abaab0df5469d9
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-19 22:53:12 +0530

    ✅ Leetcode: Find the Highest Altitude [Easy] (Python3)

---
+++ leetcode/easy/1833-find-the-highest-altitude.py
@@ -0,0 +1,23 @@
+# ===================================================
+# Problem  : Find the Highest Altitude
+# Platform : Leetcode
+# Link     : https://leetcode.com/problems/find-the-highest-altitude/
+# Difficulty: Easy
+# Language : Python3
+# Runtime  : 1 ms
+# Memory   : 19.2 MB
+# Date     : June 19, 2026
+# ===================================================class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+         n = len(gain)
+         mx = 0
+
+         for i in range(n + 1):
+            alt = 0
+            for j in range(i):
+                alt += gain[j]
+            mx = max(mx, alt)
+
+         return mx
+
+        
