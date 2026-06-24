bdfe76c2d3480ddfb8159caba5c2d9f44162762f
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-24 21:47:50 +0530

    ✅ Leetcode: Palindrome Number [Easy] (Python3)

--- leetcode/easy/0009-palindrome-number.py
+++ leetcode/easy/0009-palindrome-number.py
@@ -4,16 +4,19 @@
 # Link     : https://leetcode.com/problems/palindrome-number/
 # Difficulty: Easy
 # Language : Python3
-# Runtime  : 17 ms
-# Memory   : 19.3 MB
-# Date     : May 27, 2026
+# Runtime  : 8 ms
+# Memory   : 19.2 MB
+# Date     : June 24, 2026
 # ===================================================class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        reverse = 0
+        xcopy = x
+
+        while x > 0:
+            reverse = (reverse * 10) + (x % 10)
+            x //= 10
         
-        temp = x
-        rev = 0
-        while temp>0:
-            r= temp%10
-            temp//=10
-            rev=rev*10 +r
-        return rev==x
+        return reverse == xcopy
