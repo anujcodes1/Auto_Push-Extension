9f4eee696a8102f9d5cd5147a9b86d5e96f59e03
Anuj Mishra <anuj444mishra@gmail.com>
2026-06-26 20:06:03 +0530

    ✅ Leetcode: Contains Duplicate [Easy] (Python3)

--- leetcode/easy/0217-contains-duplicate.py
+++ leetcode/easy/0217-contains-duplicate.py
@@ -4,8 +4,8 @@
 # Link     : https://leetcode.com/problems/contains-duplicate/
 # Difficulty: Easy
 # Language : Python3
-# Runtime  : 54 ms
-# Memory   : 30 MB
+# Runtime  : 44 ms
+# Memory   : 29.9 MB
 # Date     : June 26, 2026
 # ===================================================class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
