7cc727b3ee2419bae2696b4b4ee9264d85a353ce
Anuj Mishra <anuj444mishra@gmail.com>
2026-07-07 22:24:33 +0530

    ✅ Leetcode: Add Two Integers [Easy] (Python3)

---
+++ leetcode/easy/2383-add-two-integers.py
@@ -0,0 +1,15 @@
+# ===================================================
+# Problem  : Add Two Integers
+# Platform : Leetcode
+# Link     : https://leetcode.com/problems/add-two-integers/
+# Difficulty: Easy
+# Language : Python3
+# Runtime  : 0 ms
+# Memory   : 19.3 MB
+# Date     : July 7, 2026
+# ===================================================class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        sum = num1 + num2
+
+        return sum
+        
