b4a14ce6890efe4b8af5ffb1684658699a46306a
Anuj Mishra <anuj444mishra@gmail.com>
2026-07-26 23:27:52 +0530

    ✅ Leetcode: Pascal's Triangle [Easy] (Python3)

--- leetcode/easy/0118-pascals-triangle.py
+++ leetcode/easy/0118-pascals-triangle.py
@@ -4,22 +4,22 @@
 # Link     : https://leetcode.com/problems/pascals-triangle/
 # Difficulty: Easy
 # Language : Python3
-# Runtime  : 4 ms
-# Memory   : 19.4 MB
-# Date     : June 13, 2026
+# Runtime  : 0 ms
+# Memory   : 19.2 MB
+# Date     : July 26, 2026
 # ===================================================class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-         if numRows == 0:
+        if numRows == 0:
             return []
-         if numRows == 1:
+        if numRows == 1:
             return [[1]]
         
-         prevRows = self.generate(numRows - 1)
-         newRow = [1] * numRows
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
         
-         for i in range(1, numRows - 1):
+        for i in range(1, numRows - 1):
             newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
         
-         prevRows.append(newRow)
-         return prevRows
+        prevRows.append(newRow)
+        return prevRows
         
